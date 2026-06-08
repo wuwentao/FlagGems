c1600820be7714aea9e64a54b642f745e3d1282c
103yiran <1039105206@qq.com>
2026-06-08 11:01:25 +0800

    style(backend): fix import order

--- src/flag_gems/runtime/backend/_ascend/ops/attention.py
+++ src/flag_gems/runtime/backend/_ascend/ops/attention.py
@@ -9,8 +9,8 @@ import triton.language as tl
 
 from flag_gems import runtime
 from flag_gems.config import use_c_extension
-from flag_gems.ops.flash_api import mha_fwd, mha_varlan_fwd
 from flag_gems.ops.attention import keep
+from flag_gems.ops.flash_api import mha_fwd, mha_varlan_fwd
 from flag_gems.runtime import torch_device_fn
 from flag_gems.utils import libentry, libtuner
 
